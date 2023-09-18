29ed83ebc9f193a00c43ac6694875abd89cefe1b
wanghongwei <1050838855@qq.com>
2023-09-18 15:21:43 +0800

    下拉框选择查看不同企业

--- src/views/computent/echarts/flightDiagram.vue
+++ src/views/computent/echarts/flightDiagram.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="mapBox">
+  <div class="mapBox" v-loading="Loading" element-loading-text="地图绘制中，请耐心等待..."
+    element-loading-background="rgba(0, 0, 0, 0.4)">
+    <div class="selectBox">
+      <el-select v-model="firmId" placeholder="请选择" @change="changeFirmId">
+        <el-option v-for="item in selectOptions" :key="item.value" :label="item.label" :value="item.value">
+        </el-option>
+      </el-select>
+    </div>
     <div ref="echart-map-ref" class="echarts"></div>
   </div>
 </template>
@@ -8,7 +15,6 @@
 
 import * as echarts from 'echarts';
 import { MapChart } from 'echarts/charts'
-import loadBMap from '../../../utils/baiduapi'
 import { InternationalMap } from '../../../utils/InternationalMap'
 import { InternationalOutline } from '../../../utils/InternationalOutline'
 import { nameMap } from '../../../utils/countryNameComparisonTable'
@@ -19,70 +25,126 @@ export default {
   components: {},
   data() {
     return {
-      mapEchart: null, // 地图
-      option: [],
-      barEchartSzie: { width: 'auto', height: 'auto' },
+      mapEchart: null, // 地图demo
+      option: [],  // 地图绘制文件
+      barEchartSzie: { width: 'auto', height: 'auto' }, // 地图大小
       debounceResize: null, // 重绘方法
       debounceRoam: null, // 缩放方法
-      barEchartData: {},
+      selectOptions: [],
+      firmId: null, // 选择的企业
+      Loading: false, // loading动画
     }
   },
   mounted() {
     this.mapEchart = echarts.init(this.$refs['echart-map-ref']);
     this.debounceResize = this.debounce(this.resize, 200)
     this.debounceRoam = this.debounce(this.roam, 200)
-
-    loadBMap("密钥").then(() => {
-      echarts.use(MapChart)
-      this.map()
-      window.addEventListener("resize", this.debounceResize)
-    })
+    echarts.use(MapChart)
+    window.addEventListener("resize", this.debounceResize)
+    // 世界地图
+    echarts.registerMap('MapJson', InternationalMap);
+    echarts.registerMap('OutlineJson', InternationalOutline);
+    // 中国地图
+    // echarts.registerMap('MapJson', chinaMap);
+    // echarts.registerMap('OutlineJson', chinaMapOutline);
+    this.Loading = true
+    this.getEnterpriseData()
   },
   beforeDestroy() {
     window.removeEventListener('resize', this.debounceResize)
   },
   methods: {
-    map() {
-      const colors = ['#ff0000', '#00ff00', '#0000ff', '#cccccc'];
-      this.mapEchart.resize(this.barEchartSzie)
-      // 世界地图
-      echarts.registerMap('MapJson', InternationalMap);
-      echarts.registerMap('OutlineJson', InternationalOutline);
-      // 中国地图
-      // echarts.registerMap('MapJson', chinaMap);
-      // echarts.registerMap('OutlineJson', chinaMapOutline);
-      var mapData = [
-        [
-          { "name": "美国", time: '2021-01-03', yield: 82004, "value": [-101.36929, 39.78972, 3] },
-          { "name": "葡萄牙", time: '2021-07-09', yield: 32500, "value": [-8.61881, 39.63034, 0] },
-          { "name": "菲律宾", time: '2021-04-08', yield: 34200, "value": [122.00750, 12.59588, 0] },
-          { "name": "文莱", time: '2023-12-25', yield: 2200, "value": [114.72767, 4.53528, 0] },
-          { "name": "喀麦隆", time: '2023-12-25', yield: 2070, "value": [11.75606, 3.84056, 0] },
-          { "name": "新西兰", time: '2021-05-08', yield: 72004, "value": [170.05699, -44.20668, 2] },
-          { "name": "纳米比亚", time: '2021-06-06', yield: 45200, "value": [15.74513, -22.59550, 1] },
-        ],
-        [
-          { "name": "缅甸", time: '2022-04-23', yield: 76200, "value": [95.57907, 21.56923, 3] },
-          { "name": "墨西哥", time: '2022-06-30', yield: 9200, "value": [-104.17650, 26.10325, 0] },
-          { "name": "斯里兰卡", time: '2022-02-28', yield: 23200, "value": [80.62697, 8.47649, 1] },
-          { "name": "肯尼亚", time: '2022-06-06', yield: 23400, "value": [37.90619, -0.02356, 1] },
-          { "name": "冰岛", time: '2022-09-16', yield: 2080, "value": [-18.24764, 65.06115, 0] },
-          { "name": "孟加拉国", time: '2023-12-25', yield: 21230, "value": [90.23915, 24.19725, 1] },
-          { "name": "哥伦比亚", time: '2023-12-25', yield: 2800, "value": [-73.08441, 3.49247, 0] },
-          { "name": "印度尼西亚", time: '2022-12-25', yield: 67200, "value": [119.86984, -4.32047, 2] },
-        ],
-        [
-          { "name": "圭亚那", time: '2023-12-25', yield: 200, "value": [-58.54359, 2.54955, 0] },
-          { "name": "几内亚比绍", time: '2023-12-25', yield: 22060, "value": [-14.93288, 12.04795, 1] },
-          { "name": "埃塞俄比亚", time: '2023-12-25', yield: 20650, "value": [39.58070, 8.61105, 1] },
-          { "name": "阿尔及利亚", time: '2023-12-25', yield: 2020, "value": [2.55060, 28.16212, 0] },
-          { "name": "北塞浦路斯", time: '2023-12-25', yield: 23400, "value": [33.82938, 35.35131, 1] },
-          { "name": "加拿大", time: '2023-12-25', yield: 202340, "value": [-106.34677, 56.13037, 3] },
-          { "name": "博茨瓦纳", time: '2023-12-25', yield: 20460, "value": [21.97458, -23.96469, 1] },
-          { "name": "百慕大", time: '2023-12-25', yield: 2050, "value": [-64.75050, 32.30780, 0] },
-          { "name": "阿塞拜疆", time: '2023-12-25', yield: 53, "value": [47.67639, 40.67088, 1] }
+    // 获取下拉框数据
+    getEnterpriseData() {
+      setTimeout(() => {
+        this.selectOptions = [
+          { value: '0', label: '选项1' },
+          { value: '1', label: '选项2' },
         ]
-      ]
+        this.$set(this, 'firmId', this.selectOptions[0].value)
+        this.changeFirmId()
+      }, 1000)
+    },
+    // 切换下拉框
+    changeFirmId() {
+      this.Loading = true
+      this.mapEchart.clear()
+      this.mapEchart.resize(this.barEchartSzie)
+      setTimeout(() => {
+        let data = [
+          {
+            timeData: ['2019', '2021', '2023'],
+            mapData: [
+              [
+                { "name": "美国", time: '2021-01-03', yield: 82004, "value": [-101.36929, 39.78972, 3] },
+                { "name": "葡萄牙", time: '2021-07-09', yield: 32500, "value": [-8.61881, 39.63034, 0] },
+                { "name": "菲律宾", time: '2021-04-08', yield: 34200, "value": [122.00750, 12.59588, 0] },
+                { "name": "文莱", time: '2023-12-25', yield: 2200, "value": [114.72767, 4.53528, 0] },
+                { "name": "喀麦隆", time: '2023-12-25', yield: 2070, "value": [11.75606, 3.84056, 0] },
+                { "name": "新西兰", time: '2021-05-08', yield: 72004, "value": [170.05699, -44.20668, 2] },
+                { "name": "纳米比亚", time: '2021-06-06', yield: 45200, "value": [15.74513, -22.59550, 1] },
+              ], [
+                { "name": "缅甸", time: '2022-04-23', yield: 76200, "value": [95.57907, 21.56923, 3] },
+                { "name": "墨西哥", time: '2022-06-30', yield: 9200, "value": [-104.17650, 26.10325, 0] },
+                { "name": "斯里兰卡", time: '2022-02-28', yield: 23200, "value": [80.62697, 8.47649, 1] },
+                { "name": "肯尼亚", time: '2022-06-06', yield: 23400, "value": [37.90619, -0.02356, 1] },
+                { "name": "冰岛", time: '2022-09-16', yield: 2080, "value": [-18.24764, 65.06115, 0] },
+                { "name": "孟加拉国", time: '2023-12-25', yield: 21230, "value": [90.23915, 24.19725, 1] },
+                { "name": "哥伦比亚", time: '2023-12-25', yield: 2800, "value": [-73.08441, 3.49247, 0] },
+                { "name": "印度尼西亚", time: '2022-12-25', yield: 67200, "value": [119.86984, -4.32047, 2] },
+              ], [
+                { "name": "圭亚那", time: '2023-12-25', yield: 200, "value": [-58.54359, 2.54955, 0] },
+                { "name": "几内亚比绍", time: '2023-12-25', yield: 22060, "value": [-14.93288, 12.04795, 1] },
+                { "name": "埃塞俄比亚", time: '2023-12-25', yield: 20650, "value": [39.58070, 8.61105, 1] },
+                { "name": "阿尔及利亚", time: '2023-12-25', yield: 2020, "value": [2.55060, 28.16212, 0] },
+                { "name": "北塞浦路斯", time: '2023-12-25', yield: 23400, "value": [33.82938, 35.35131, 1] },
+                { "name": "加拿大", time: '2023-12-25', yield: 202340, "value": [-106.34677, 56.13037, 3] },
+                { "name": "博茨瓦纳", time: '2023-12-25', yield: 20460, "value": [21.97458, -23.96469, 1] },
+                { "name": "百慕大", time: '2023-12-25', yield: 2050, "value": [-64.75050, 32.30780, 0] },
+                { "name": "阿塞拜疆", time: '2023-12-25', yield: 53, "value": [47.67639, 40.67088, 1] }
+              ]
+            ]
+          },
+          {
+            timeData: ['2021', '2022', '2023'],
+            mapData: [
+              [
+                { "name": "美国", time: '2021-01-03', yield: 82004, "value": [-101.36929, 39.78972, 3] },
+                { "name": "葡萄牙", time: '2021-07-09', yield: 32500, "value": [-8.61881, 39.63034, 0] },
+                { "name": "菲律宾", time: '2021-04-08', yield: 34200, "value": [122.00750, 12.59588, 0] },
+                { "name": "文莱", time: '2023-12-25', yield: 2200, "value": [114.72767, 4.53528, 0] },
+                { "name": "喀麦隆", time: '2023-12-25', yield: 2070, "value": [11.75606, 3.84056, 0] },
+                { "name": "新西兰", time: '2021-05-08', yield: 72004, "value": [170.05699, -44.20668, 2] },
+                { "name": "纳米比亚", time: '2021-06-06', yield: 45200, "value": [15.74513, -22.59550, 1] },
+              ], [
+                { "name": "缅甸", time: '2022-04-23', yield: 76200, "value": [95.57907, 21.56923, 3] },
+                { "name": "墨西哥", time: '2022-06-30', yield: 9200, "value": [-104.17650, 26.10325, 0] },
+                { "name": "斯里兰卡", time: '2022-02-28', yield: 23200, "value": [80.62697, 8.47649, 1] },
+                { "name": "肯尼亚", time: '2022-06-06', yield: 23400, "value": [37.90619, -0.02356, 1] },
+                { "name": "冰岛", time: '2022-09-16', yield: 2080, "value": [-18.24764, 65.06115, 0] },
+                { "name": "孟加拉国", time: '2023-12-25', yield: 21230, "value": [90.23915, 24.19725, 1] },
+                { "name": "哥伦比亚", time: '2023-12-25', yield: 2800, "value": [-73.08441, 3.49247, 0] },
+                { "name": "印度尼西亚", time: '2022-12-25', yield: 67200, "value": [119.86984, -4.32047, 2] },
+              ], [
+                { "name": "圭亚那", time: '2023-12-25', yield: 200, "value": [-58.54359, 2.54955, 0] },
+                { "name": "几内亚比绍", time: '2023-12-25', yield: 22060, "value": [-14.93288, 12.04795, 1] },
+                { "name": "埃塞俄比亚", time: '2023-12-25', yield: 20650, "value": [39.58070, 8.61105, 1] },
+                { "name": "阿尔及利亚", time: '2023-12-25', yield: 2020, "value": [2.55060, 28.16212, 0] },
+                { "name": "北塞浦路斯", time: '2023-12-25', yield: 23400, "value": [33.82938, 35.35131, 1] },
+                { "name": "加拿大", time: '2023-12-25', yield: 202340, "value": [-106.34677, 56.13037, 3] },
+                { "name": "博茨瓦纳", time: '2023-12-25', yield: 20460, "value": [21.97458, -23.96469, 1] },
+                { "name": "百慕大", time: '2023-12-25', yield: 2050, "value": [-64.75050, 32.30780, 0] },
+                { "name": "阿塞拜疆", time: '2023-12-25', yield: 53, "value": [47.67639, 40.67088, 1] }
+              ]
+            ]
+          }
+        ]
+        this.map(data[this.firmId].timeData, data[this.firmId].mapData)
+      }, 1000)
+    },
+    // 地图绘制
+    map(timeData, mapData) {
+      const colors = ['#ff0000', '#00ff00', '#0000ff', '#cccccc'];
       const coverData = (startcoord, mapData) => {
         let res = mapData.map((item, index) => {
           return {
@@ -104,8 +166,8 @@ export default {
         baseOption: {
           timeline: {
             autoPlay: false,
-            currentIndex: 2,
-            data: ['2021', '2022', '2023'],
+            currentIndex: timeData.length - 1,
+            data: timeData,
           },
           geo: [{
             silent: false,
@@ -384,6 +446,7 @@ export default {
           })
       });
       this.option && this.mapEchart.setOption(this.option);
+      this.Loading = false
       if (this.option) {
         this.mapEchart.on('georoam', (params) => {
           var option = JSON.parse(JSON.stringify(this.mapEchart.getOption()));
@@ -433,7 +496,6 @@ export default {
               }
             }))
           ]
-          // 执行相应的操作
           this.mapEchart.setOption(this.option);
         });
       }
@@ -456,6 +518,7 @@ export default {
       this.mapEchart.clear()
       this.option && this.mapEchart.setOption(this.option);
     },
+    // 地图收缩
     roam() {
       var option = this.option;
       var options = JSON.parse(JSON.stringify(this.mapEchart.getOption()));
@@ -473,20 +536,19 @@ export default {
   width: 100%;
   overflow-x: auto;
   overflow-y: hidden;
-}
 
-.echarts {
-  /* width: 100%; */
-  min-width: 1700px;
-  height: 100%;
-}
+  .selectBox {
+    position: absolute;
+    top: 10px;
+    left: 50px;
+    z-index: 99;
+  }
 
-.barChart {
-  position: absolute;
-  right: 0;
-  top: 0;
-  width: 300px;
-  height: 300px;
+  .echarts {
+    /* width: 100%; */
+    min-width: 1700px;
+    height: 100%;
+  }
 }
 
 ::v-deep .scatterTooltip {
