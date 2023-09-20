0b10d93e585b3aa84f0996017876d636b7d37872
wanghongwei <1050838855@qq.com>
2023-09-20 18:03:25 +0800

    申述页面

--- src/views/industrialRelief/index.vue
+++ src/views/industrialRelief/index.vue
@@ -1,8 +1,12 @@
 <template>
   <div>
-    <panel :panelHeaderShow="true" :panelAsideWidth="'20vw'">
+    <panel :panelHeaderShow="true">
       <template #aside>
-        <div class="borderBox"></div>
+        <div class="borderBox">
+          <el-button type="primary" :disabled="stepActive < 0" @click="stepActive = 0">基本信息填报</el-button>
+          <el-button type="primary" :disabled="stepActive < 1" @click="stepActive = 1">受打压申请</el-button>
+          <el-button type="primary" :disabled="stepActive < 2" @click="stepActive = 2">产业生态扶持</el-button>
+        </div>
       </template>
       <template #main>
         <el-steps class="stepsBox" :space="160" :active="stepActive" finish-status="success" process-status="finish"
@@ -19,19 +23,29 @@
             <el-button type="primary" @click="submit">提交</el-button>
           </template>
         </vFrom>
-        <vFrom v-if="stepActive == 1" labelWidth="140px" ref="beCrushedRef" size="default" :searchData="beCrushedData"
-          :searchForm="beCrushedForm" :cancelBtn="false" :successBtn="false" @fromSubmit="onSubmit">
-          <template #button>
-            <el-button type="primary" @click="save">保存</el-button>
-            <el-button type="primary" @click="submit">提交</el-button>
+        <div v-if="stepActive == 1">
+          <template v-for="(item, index) in beCrushedData">
+            <vFrom :key="'vF' + index" labelWidth="140px" :ref="'beCrushedRef' + index" size="default" :searchData="item"
+              :searchForm="beCrushedForm" :cancelBtn="false" :successBtn="false" @fromSubmit="onSubmit">
+              <template #button v-if="index == (beCrushedData.length - 1)">
+                <el-button type="primary" @click="addBeCrushedData">新增打压申请</el-button>
+                <el-button type="primary" @click="save">保存</el-button>
+                <el-button type="primary" @click="submit">提交</el-button>
+              </template>
+              <template #button v-else>
+                <el-button type="primary" @click="delectBeCrushedData(index)">删除</el-button>
+                <el-divider class="divider"></el-divider>
+              </template>
+            </vFrom>
           </template>
-        </vFrom>
+        </div>
         <div v-if="stepActive == 2">
           <vDescriptions class="searchInput" ref="searchSubmitForm" size="default" :searchData="basicInformationData"
             :searchForm="basicInformationForm" :title="'基本信息'">
           </vDescriptions>
-          <vDescriptions class="searchInput" ref="searchSubmitForm" size="default" :searchData="beCrushedData"
-            :searchForm="beCrushedForm" :title="'受打压申请'" :column="1">
+          <vDescriptions v-for="(item, index) in beCrushedData" :key="'vD' + index" class="searchInput"
+            ref="searchSubmitForm" size="default" :searchData="item" :searchForm="beCrushedForm" :title="'受打压申请'"
+            :column="1">
           </vDescriptions>
           <el-button type="primary" @click="$router.push('/index')">返回主页</el-button>
           <el-button type="primary" @click="toggleStepBar">重新填写</el-button>
@@ -53,14 +67,16 @@ export default {
     return {
       stepActive: 0,
       basicInformationData: {}, // 基本信息填报数据
-      beCrushedData: {}, // 受打压申请数据
+      beCrushedData: [{}], // 受打压申请数据
       basicInformationForm: [], // 基本信息填报表单
       beCrushedForm: [], // 受打压申请表单
+      throttleStep: null, // 节流
     }
   },
   async created() {
     this.basicInformationForm = await basicInformationForm().then(res => { return res })
     this.beCrushedForm = await beCrushedForm().then(res => { return res })
+    this.throttleStep = this.throttle(this.toggleStepBar, 200)
   },
   methods: {
     // 保存
@@ -70,19 +86,45 @@ export default {
     // 提交
     submit() {
       let ref = { 0: 'basicInformationRef', 1: 'beCrushedRef' }
-      this.$refs[ref[this.stepActive]].onSubmit('searchFormRef')
+      if (this.stepActive == 0) {
+        this.$refs['basicInformationRef'].onSubmit('searchFormRef')
+      } else {
+        this.beCrushedData.forEach((item, index) => {
+          this.$refs['beCrushedRef' + index][0].onSubmit('searchFormRef')
+        })
+      }
     },
     // 验证结束
     onSubmit(item) {
-      console.log(item);
-      this.toggleStepBar()
+      console.log(this.beCrushedData);
+      this.throttleStep()
     },
     // 切换步骤栏
     toggleStepBar() {
       // let ref = { 0: 'basicInformationRef', 1: 'beCrushedRef' }
       // this.$refs[ref[this.stepActive]].onCancel('searchFormRef')
       if (this.stepActive++ > 1) this.stepActive = 0;
-    }
+    },
+    // 新增打压申请
+    addBeCrushedData() {
+      this.beCrushedData.length < 3 && this.beCrushedData.push({})
+    },
+    // 删除打压申请
+    delectBeCrushedData(index) {
+      this.beCrushedData.splice(index, 1)
+    },
+    // 节流
+    throttle(fn, delay) {
+      let timer = null;
+      return function () {
+        if (!timer) {
+          timer = setTimeout(() => {
+            fn.apply(this, arguments);
+            timer = null;
+          }, delay);
+        }
+      };
+    },
   },
 }
 </script>
@@ -99,9 +141,11 @@ export default {
 
 .borderBox {
   position: relative;
-  width: 100%;
-  height: 100%;
+  height: calc(100% - 50px);
+  padding: 50px 40px 0;
   z-index: 1;
+  display: flex;
+  flex-direction: column;
 
   &::before {
     content: "";
@@ -115,6 +159,15 @@ export default {
     background-size: 100% 100%;
     z-index: -1;
   }
+
+  ::v-deep .el-button+.el-button {
+    margin-left: 0px;
+    margin-top: 10px;
+  }
+}
+
+.divider {
+  width: 70vw
 }
 </style>
     
